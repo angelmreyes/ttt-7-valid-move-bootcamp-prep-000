4a4e330c5b5ce092c5e4f13132614d5fc579d505
loading-torrent-9632 <angelrey@buffalo.edu>
2019-04-18 01:40:51 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,6 @@
 # code your #valid_move? method here
 def valid_move?(board, index)
-  if index >= 1 && index <= 9
-    true
-  end
+  index >= 1 && index <= 9 && !position_taken?(board, index)
 end
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
